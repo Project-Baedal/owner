d23833adaf3da098e80573a75939f99cc5613661
handsone-u <handsoneu@gmail.com>
2025-04-28 13:22:29 +0900

    [#3] add: 매장 추가 API 보안 적용

--- src/main/java/com/baedal/owner/adapter/in/web/controller/OwnerController.java
+++ src/main/java/com/baedal/owner/adapter/in/web/controller/OwnerController.java
@@ -14,6 +14,8 @@ import com.baedal.owner.application.port.in.OwnerUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,11 +45,12 @@ public class OwnerController {
     return ResponseEntity.ok(mapper.signupToResponse(commandResponse));
   }
 
+  @PreAuthorize("hasRole('OWNER')")
   @PostMapping("/v0/addStore")
-  public ResponseEntity<Void> addStore(@RequestBody AddStoreRequest req) {
+  public ResponseEntity<Void> addStore(
+      @RequestBody AddStoreRequest req,
+      @AuthenticationPrincipal Long ownerId) {
     AddStoreCommand.Request command = mapper.addStoreToCommand(req);
-    // note. 인증 관련 코드 작성 이후 변경 예정
-    Long ownerId = 1L;
     ownerUseCase.addStore(ownerId, command);
     return ResponseEntity.noContent().build();
   }
